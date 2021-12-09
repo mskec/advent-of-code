f966365264ec194999effae6844b88a3ef27e730
mskec <martin.skec@gmail.com>
2021-12-09 10:32:32 +0100

    task(21-09-2)

--- 2021/day09/day09-2.js
+++ 2021/day09/day09-2.js
@@ -1,4 +1,4 @@
-// https://adventofcode.com/2021/day/9
+// https://adventofcode.com/2021/day/9#part2
 
 const path = require('path');
 const fs = require('fs');
@@ -6,7 +6,7 @@ const fs = require('fs');
 const isLowPoint = (map, i, j) => {
   let isLowest = true;
   let point = map[j][i];
-  const debug = [];
+  // const debug = [];
   // debug.push(['isLowPoint', i, j]);
   // up
   if (j-1 >= 0) {
@@ -36,6 +36,33 @@ const isLowPoint = (map, i, j) => {
   return isLowest;
 };
 
+const calcBasinSize = (map, i, j, visited = {}) => {
+  if (map[j][i] === 9 || visited[`${i},${j}`]) {
+    return 0;
+  }
+
+  visited[`${i},${j}`] = true;
+
+  let size = 1;
+  if (j-1 >= 0) {
+    size += calcBasinSize(map, i, j-1, visited);
+  }
+  // right
+  if (i+1 <= map[j].length-1) {
+    size += calcBasinSize(map, i+1, j, visited);
+  }
+  // down
+  if (j+1 <= map.length-1) {
+    size += calcBasinSize(map, i, j+1, visited);
+  }
+  // left
+  if (i-1 >= 0) {
+    size += calcBasinSize(map, i-1, j, visited);
+  }
+
+  return size;
+};
+
 run(
   fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8').trim()
 );
@@ -45,16 +72,18 @@ function run(input) {
 
   const map = mapRaw.map(row => row.split('').map(n => +n));
 
-  let lowPoints = [];
+  let basinSizes = [];
   for (let j = 0; j < map.length; j++) {
     for (let i = 0; i < map[j].length; i++) {
       if (isLowPoint(map, i, j)) {
-        lowPoints.push(1 + map[j][i]);
+        basinSizes.push(calcBasinSize(map, i, j));
       }
     }
   }
 
-  // console.log({ lowPoints });
-  let sum = lowPoints.reduce((agg, n) => agg + n, 0);
+  basinSizes.sort((a, b) => b - a);
+  // console.log({ basinSizes });
+  // top 3
+  let sum = basinSizes.slice(0, 3).reduce((agg, n) => agg * n, 1);
   console.log('result:', sum);
 }
