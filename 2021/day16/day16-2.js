7d208957a471b6bd3964fe191a8aa8b43eca7960
mskec <martin.skec@gmail.com>
2021-12-16 23:36:44 +0100

    task(21-16-2)

--- 2021/day16/day16-2.js
+++ 2021/day16/day16-2.js
@@ -1,9 +1,7 @@
-// https://adventofcode.com/2021/day/16
+// https://adventofcode.com/2021/day/16#part2
 
 const path = require('path');
 const fs = require('fs');
-
-let versionSum = 0;
 
 run(
   fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8').trim()
@@ -24,22 +22,68 @@ function toDecimal(bin) {
   return parseInt(bin, 2);
 }
 
+function processOperator(type, values) {
+  switch(type) {
+    case 0: {
+      // sum operator
+      return values.reduce((agg, val) => agg + val, 0);
+    }
+    case 1: {
+      // product operator
+      return values.reduce((agg, val) => agg * val, 1);
+    }
+    case 2: {
+      // min operator
+      let min = Number.MAX_VALUE;
+      values.forEach(val => {
+        if (val < min) {
+          min = val;
+        }
+      });
+      return min;
+    }
+    case 3: {
+      // max operator
+      let max = Number.MIN_VALUE;
+      values.forEach(val => {
+        if (val > max) {
+          max = val;
+        }
+      });
+      return max;
+    }
+    case 5: {
+      return values[0] > values[1] ? 1 : 0;
+    }
+    case 6: {
+      return values[0] < values[1] ? 1 : 0;
+    }
+    case 7: {
+      return values[0] === values[1] ? 1 : 0;
+    }
+    default: {
+      throw Error(`unknown operator type ${type}`);
+    }
+  }
+}
+
 function parsePacket(ptr, bin, inOperator = false) {
   let packetReadCnt = 0;
+  let value = 0;
   const [version, type, readCnt] = parseHeader(ptr + packetReadCnt, bin);
   packetReadCnt += readCnt;
-  versionSum += version;
 
   console.log('> version:', version, 'type:', type);
 
   if (Number.isNaN(version) || Number.isNaN(type)) {
-    console.log('stop');
+    console.log('< stop');
     return [packetReadCnt];
   } else if (type === 4) {
     // parse literal packet
-    const [value, readCnt] = parseLiteralPacket(ptr + packetReadCnt, bin, !inOperator);
+    const [literalValue, readCnt] = parseLiteralPacket(ptr + packetReadCnt, bin, !inOperator);
     packetReadCnt += readCnt;
-    console.log('> literal packet:', value);
+    value = literalValue;
+    console.log('> literal packet:', literalValue);
 
   } else {
     // parse operator packet
@@ -47,28 +91,32 @@ function parsePacket(ptr, bin, inOperator = false) {
     packetReadCnt += readCnt;
     console.log('> operator packet:', lengthTypeId, 'num:', num);
 
+    const subValues = [];
     if (lengthTypeId === 0) {
       let subReadCnt = 0;
       while (subReadCnt < num) {
         console.log('> sub packet pointer:', ptr + packetReadCnt);
-        const [readCnt] = parsePacket(ptr + packetReadCnt, bin, true);
+        const [subPacketValue, readCnt] = parsePacket(ptr + packetReadCnt, bin, true);
         subReadCnt += readCnt;
         packetReadCnt += readCnt;
+        subValues.push(subPacketValue);
       }
     } else if (lengthTypeId === 1) {
       let subPacketsRead = 0;
       while (subPacketsRead < num) {
         console.log('> sub packet pointer:', ptr + packetReadCnt);
-        const [readCnt] = parsePacket(ptr + packetReadCnt, bin, true);
+        const [subPacketValue, readCnt] = parsePacket(ptr + packetReadCnt, bin, true);
         subPacketsRead += 1;
         packetReadCnt += readCnt;
+        subValues.push(subPacketValue);
       }
     } else {
       throw Error(`unknown length type: ${num}`)
     }
+    value = processOperator(type, subValues);
   }
 
-  return [packetReadCnt];
+  return [value, packetReadCnt];
 }
 
 function parseHeader(ptr, bin) {
@@ -119,12 +167,14 @@ function run(input) {
 
   let ptr = 0;
   let packet = 0;
+  const result = [];
   while (bin.length - ptr > 6) {
     packet++;
     console.log('packet:', packet, 'pointer:', ptr);
-    const [readCnt] = parsePacket(ptr, bin);
+    const [value, readCnt] = parsePacket(ptr, bin);
     ptr += readCnt;
+    result.push(value);
   }
 
-  console.log('version sum:', versionSum);
+  console.log('result', result);
 }
